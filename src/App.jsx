0db410a0325f8df07aca084b364bc534655a3378
Ondřej Hájek <ondra.hajku@seznam.cz>
2023-04-16 08:41:07 +0200

    vercel deploy test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='w-screen h-screen flex flex-col'>
       <div className='flex m-6'>
-        <h1 className='text-3xl'>Homba za pokladem</h1>
+        <h1 className='text-3xl'>Homba za pokladem DEPLOY TEST</h1>
       </div>
 
       <div className='flex-1 bg-red-500 flex'>
