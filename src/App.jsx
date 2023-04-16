223c68a3da989da345bd4c196ea433389c8340c5
Ondřej Hájek <ondra.hajku@seznam.cz>
2023-04-16 08:23:50 +0200

    init tailwind

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className='w-screen h-screen flex flex-col'>
+      <div className='flex m-6'>
+        <h1 className='text-3xl'>Homba za pokladem</h1>
+      </div>
+
+      <div className='flex-1 bg-red-500 flex'>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
